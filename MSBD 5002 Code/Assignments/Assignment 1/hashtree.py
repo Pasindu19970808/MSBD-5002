e4e8e1399504a27e383d92641f0fee97d93fc175
Pasindu N. Samaranayake <54874939+Pasindu19970808@users.noreply.github.com>
2021-09-21 23:20:15 +0800

    hash tree input sorting

--- MSBD 5002 Code/Assignments/Assignment 1/hashtree.py
+++ MSBD 5002 Code/Assignments/Assignment 1/hashtree.py
@@ -4,10 +4,40 @@ import re
 import copy
 #candidates = '{1 2 3 }, {1 4 5}, {1 2 4}, {1 2 5}, {1 5 9}, {1 3 6},{2 3 4}, {2 5 9},{3 4 5}, {3 5 6}, {3 5 9}, {3 8 9}, {3 2 6},{4 5 7}, {4 1 8}, {4 7 8}, {4 6 7},{6 1 3}, {6 3 4}, {6 8 9}, {6 2 1}, {6 4 3}, {6 7 9},{8 2 4}, {8 9 1}, {8 3 6}, {8 3 7}, {8 4 7}, {8 5 1}, {8 3 1}, {8 6 2}'
 
-candidates = '{1 4 5}, {1 2 4}, {4 5 7}, {1 2 5}, {4 5 8}, {1 5 9}, {1 3 6}, {2 3 4}, {5 6 7},{3 4 5}, {3 5 6}, {3 5 7}, {6 8 9}, {3 6 7}, {3 6 8}'
-res = [int(i) for i in re.findall(r'[^{},]',candidates) if i != ' ']
-interval = int(len(res)/3)
-res_final = [res[3*int(i):3*int(i+1)] for i in range(0,interval)]
+#candidates = '{1 4 5}, {1 2 4}, {4 5 7}, {1 2 5}, {4 5 8}, {1 5 9}, {1 3 6}, {2 3 4}, {5 6 7},{3 4 5}, {3 5 6}, {3 5 7}, {6 8 9}, {3 6 7}, {3 6 8}'
+#res = [int(i) for i in re.findall(r'[^{},]',candidates) if i != ' ']
+#interval = int(len(res)/3)
+#res_final = [res[3*int(i):3*int(i+1)] for i in range(0,interval)]
+
+
+candidate_set = [[1, 2, 3],
+ [1, 4, 5],
+ [1, 2, 4],
+ [1, 2, 5],
+ [1, 5, 9],
+ [1, 3, 6],
+ [2, 3, 4],
+ [2, 5, 9],
+ [3, 4, 5],
+ [3, 5, 6],
+ [3, 5, 9],
+ [3, 8, 9],
+ [2, 3, 6],
+ [4, 5, 7],
+ [1, 4, 8],
+ [4, 7, 8],
+ [4, 6, 7],
+ [3, 4, 6],
+ [6, 8, 9],
+ [1, 2, 6],
+ [6, 7, 9],
+ [2, 4, 8],
+ [1, 8, 9],
+ [3, 6, 8],
+ [3, 7, 8],
+ [1, 5, 8],
+ [1, 3, 8],
+ [2, 6, 8]]
 
 class root:
     def __init__(self):
@@ -89,7 +119,7 @@ def output_hashtree(result,node = None):
                 output_hashtree(result=result[i])
 
 rootnode = root()
-create_hashtree(res_final,node = rootnode, sort = False, max_leafsize = 3,depth = 0)
+create_hashtree(candidate_set,node = rootnode, sort = False, max_leafsize = 3,depth = 0)
 final_result = list()
 output_hashtree(final_result,node=rootnode)
 print(final_result)
